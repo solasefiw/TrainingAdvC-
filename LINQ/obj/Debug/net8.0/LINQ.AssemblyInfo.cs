8dfb8342db4f92c52ae5b1fa534a1b82a7b4a812
Solomon S. <125763152+solasefiw@users.noreply.github.com>
2024-07-10 11:45:16 +0300

    first commut

--- LINQ/obj/Debug/net8.0/LINQ.AssemblyInfo.cs
+++ LINQ/obj/Debug/net8.0/LINQ.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LINQ")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6bb4acef5a9312f5f694149ad4a4897fb417e82f")]
 [assembly: System.Reflection.AssemblyProductAttribute("LINQ")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LINQ")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
